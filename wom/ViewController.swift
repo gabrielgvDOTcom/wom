4993e42c9038475ad47ea3d9a3897d85f9bead2b
Gabriel Gárate Vivanco <hola@gabrielgv.com>
2024-03-26 09:56:38 -0300

    Initial Commit

---
+++ wom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wom
+//
+//  Created by Gabriel Gárate Vivanco on 26-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
